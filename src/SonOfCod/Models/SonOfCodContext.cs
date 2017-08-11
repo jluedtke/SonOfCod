39aa3a2c9b7adac673efe2c7d18c1fa0e8ba5de9
Jared Luedtke <jared7223@hotmail.com>
2017-08-11 09:38:36 -0700

    add initial migration and set up User Authentication

---
+++ src/SonOfCod/Models/SonOfCodContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SonOfCod.Models;
+
+namespace SonOfCod.Models
+{
+    public class SonOfCodContext : IdentityDbContext<ApplicationUser>
+    {
+        public SonOfCodContext()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=SonOfCod;integrated security=True");
+        }
+        public SonOfCodContext(DbContextOptions<SonOfCodContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+    }
+}
