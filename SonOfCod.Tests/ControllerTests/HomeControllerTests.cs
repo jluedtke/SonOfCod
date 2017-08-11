7e99de6c1c7f45e2ff3e3b3e6b236198bccb8c7e
Jared Luedtke <jared7223@hotmail.com>
2017-08-11 12:29:13 -0700

    add model for recipients and set up initial testing for home controller

---
+++ SonOfCod.Tests/ControllerTests/HomeControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using SonOfCod.Controllers;
+using SonOfCod.Models;
+using Xunit;
+
+namespace SonOfCod.Tests.ControllerTests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void Get_ViewResult_Index_Test()
+        {
+            //Arrange
+            HomeController controller = new HomeController();
+
+            //Act
+            var result = controller.Index();
+
+            //Assert
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
---
+++ src/SonOfCod/Models/Recipient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SonOfCod.Models
+{
+    [Table("Recipient")]
+    public class Recipient
+    {
+        [Key]
+        private int RecipeintId { get; set; }
+        private string Email { get; set; }
+        private string FirstName { get; set; }
+        private string LastName { get; set; }
+        private string PhoneNumber { get; set; }
+    }
+}
